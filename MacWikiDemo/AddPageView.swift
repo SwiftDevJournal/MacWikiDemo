180733c89309eea28a1a97ce7d0d851cff143847
Mark Szymczyk <mark@swiftdevjournal.com>
2021-08-20 15:02:02 -0400

    Add a SwiftUI view to create a new page.

---
+++ MacWikiDemo/AddPageView.swift
@@ -0,0 +1,20 @@
+//
+//  AddPageView.swift
+//  MacWikiDemo
+//
+//  Created by mark on 8/20/21.
+//
+
+import SwiftUI
+
+struct AddPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddPageView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddPageView()
+    }
+}
