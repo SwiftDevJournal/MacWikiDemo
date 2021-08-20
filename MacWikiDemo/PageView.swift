d7a8e1431599f2ffd0abd59d42eaa00741f75d64
Mark Szymczyk <mark@swiftdevjournal.com>
2021-08-20 14:57:47 -0400

    Add a page view to show the selected page.

---
+++ MacWikiDemo/PageView.swift
@@ -0,0 +1,20 @@
+//
+//  PageView.swift
+//  MacWikiDemo
+//
+//  Created by mark on 8/20/21.
+//
+
+import SwiftUI
+
+struct PageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PageView_Previews: PreviewProvider {
+    static var previews: some View {
+        PageView()
+    }
+}
