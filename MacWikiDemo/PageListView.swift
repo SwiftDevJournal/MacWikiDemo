9e7e727a5fae88213acc25012dd2df28a9aa4cb6
Mark Szymczyk <mark@swiftdevjournal.com>
2021-08-20 15:03:17 -0400

    Add a view to show the list of pages in the document.

---
+++ MacWikiDemo/PageListView.swift
@@ -0,0 +1,20 @@
+//
+//  PageListView.swift
+//  MacWikiDemo
+//
+//  Created by mark on 8/20/21.
+//
+
+import SwiftUI
+
+struct PageListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PageListView_Previews: PreviewProvider {
+    static var previews: some View {
+        PageListView()
+    }
+}
